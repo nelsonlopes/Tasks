899d790e22357f243f3e2b4c85e121412a655b07
Nelson Lopes <nelopes@expressglass.pt>
2020-01-12 10:25:34 +0000

    feat: Add Task model

---
+++ app/src/main/java/com/nelsonlopes/tasks/models/Task.java
@@ -0,0 +1,47 @@
+package com.nelsonlopes.tasks.models;
+
+public class Task {
+    private String documentId;
+    private String name;
+    private String projectId;
+    private String userUid;
+
+    // CONSTRUCTORS
+    public Task() {
+
+    }
+
+    // GETTERS
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public String getUserUid() {
+        return userUid;
+    }
+
+    // SETTERS
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+
+    public void setUserUid(String userUid) {
+        this.userUid = userUid;
+    }
+}
